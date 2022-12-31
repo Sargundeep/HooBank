4cbadfab54ead1718b07c104a190b2291e9f9cad
Sargundeep <sargundeepkaur.s@somaiya.edu>
2022-12-31 16:27:13 +0530

    feat: final commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import FeedbackCard from "./FeedbackCard";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+export {
+    Navbar,
+    Clients,
+    CTA,
+    Hero,
+    Stats,
+    Testimonials,
+    FeedbackCard,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    GetStarted,
+    Footer
+};
+
